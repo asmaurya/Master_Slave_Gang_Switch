bff99a0bb2f5655777a7be4c219c675cb11bcfb8
Abhishek Singh Maurya <asmaurya.in@gmail.com>
2019-07-11 02:35:46 +0530

    Add project files.

---
+++ Master_Slave_Gang_Switch/Master_Slave_Gang_Switch.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Master_Slave_Gang_Switch.ino
+ Created:	7/11/2019 2:35:38 AM
+ Author:	Abhishek
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
